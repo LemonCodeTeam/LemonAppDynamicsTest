bba390cad522cdfc1c3d84cbac776127ed5b1a7d
TwilightLemon <cz241126@live.com>
2023-06-13 14:40:47 +0800

    添加项目文件。

---
+++ LemonAppDynamicsTest/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Controls;
+
+namespace LemonAppDynamicsTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public static SocketsHttpHandler GetSta() => new SocketsHttpHandler()
+        {
+            AutomaticDecompression = DecompressionMethods.GZip,
+            KeepAlivePingPolicy = HttpKeepAlivePingPolicy.Always
+        };
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using var hc = new HttpClient(GetSta());
+            var a = await hc.GetAsync("https://gitee.com/TwilightLemon/LemonAppDynamics/raw/master/Windows_LemonApp_AboutPage.xaml");
+            string data= await a.Content.ReadAsStringAsync();
+            cons.Children.Add((Grid)XamlReader.Parse(data));
+        }
+    }
+}
